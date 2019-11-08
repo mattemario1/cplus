47fee345806e4b06f42c34585ea43c15afc4c9b3
mattemario1 <sirmathioso@gmail.com>
2019-11-08 11:24:07 +0100

    Filer från windows

---
+++ Momstabell.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;	//Säger att man vill använda paketet std.
+int main()
+{
+	double Fp{}; //Ger värdet 0.0 till decimalen Fp
+	cout << "Mata in första pris (Mellan 0 och 100): ";
+	cin >> Fp; 
+	while (Fp < 0.0 || Fp > 100.0)	// || är samma som or
+	{
+		cout << "Error: Mata in flrsta pris igen ";
+		cin >> Fp;
+	}
+	double Lp{}, S{};
+	do	//Det är en loop
+	{
+		cout << "Mata in sista pris (minst första pris): ";
+		cin >> Lp;
+	} while (Lp < Fp);
+	cout << "Mata in steglängd";
+	for(;;)
+	{
+		cin >> S;
+		if (S > 0.0)
+		{
+			break;
+		}
+	}
+	double M{};
+	M = 25.0;
+	
+	int n;
+	n = floor ((Lp - Fp) / S + 1.0);
+}
